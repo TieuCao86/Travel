1497e020b34301fbfa5400bcd1851c96eb62a9d1
TieuCao86 <caotrung862005@gmail.com>
2026-01-04 19:27:26 +0700

    fix transport

---
+++ travel/src/main/resources/static/js/constants/transport.constants.js
@@ -0,0 +1,17 @@
+const TRANSPORT_ICON_MAP = {
+    BUS: "fa-bus",
+    PLANE: "fa-plane-departure",
+    TRAIN: "fa-train",
+    SHIP: "fa-ship",
+    MOTORBIKE: "fa-motorcycle",
+    CAR: "fa-car"
+};
+
+const TRANSPORT_LABEL_MAP = {
+    BUS: "Xe du lịch",
+    PLANE: "Máy bay",
+    TRAIN: "Tàu hỏa",
+    SHIP: "Tàu thủy",
+    MOTORBIKE: "Xe máy",
+    CAR: "Ô tô"
+};
